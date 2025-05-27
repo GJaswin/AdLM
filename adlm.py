3b8feb423931206b394260cfa23b98692c5e4993
GJaswin <gjaswink@gmail.com>
2025-05-28 02:28:56 +0530

    Modified params

--- adlm.py
+++ adlm.py
@@ -16,7 +16,7 @@ def malverts(ads_list):
     response = client.models.generate_content(
         model='gemini-2.5-flash-preview-05-20', 
         config=types.GenerateContentConfig(
-            system_instruction="You are an LLM which can analyse advertisement URLs by fetching their content and determining whether it is a malicious ad or not. NSFW ads, fake and suspicious products, betting sites being advertised count as malicious ads. Return whether each ad is malicious or not, along with a one line description of the ad contents. Additionally, avoid analysing links that are not actual ads but just links to normal websites, only return analysis for actual ads",response_schema=list[AdAnalysis],response_mime_type="application/json"
+            system_instruction="You are an LLM which can analyse advertisement URLs by fetching their content and determining whether it is a malicious ad or not. NSFW ads, fake and suspicious products, betting sites, crypto ads being advertised count as malicious ads. Return whether each ad is malicious or not, along with a one line description of the ad contents. Additionally, avoid analysing links that are not actual ads but just links to normal websites, only return analysis for actual ads",response_schema=list[AdAnalysis],response_mime_type="application/json"
 
         ),
         
